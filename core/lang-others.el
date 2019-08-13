c6a1d7a9f5bda4e8124de6f3014fb97edb687ff9
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-08-13 22:32:24 +0700

    Upgrade planuml-mode by setting default exec mode

--- core/lang-others.el
+++ core/lang-others.el
@@ -3,7 +3,9 @@
 
 (setq plantuml-jar-path "~/org-modes/plantuml.jar")
 (use-package plantuml-mode
-  :commands plantuml-mode)
+  :commands plantuml-mode
+  :custom
+  (plantuml-default-exec-mode 'jar))
 
 (use-package apib-mode
   :mode ("\\.apib\\'" . apib-mode))
